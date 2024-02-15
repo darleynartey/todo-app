c5ccfa12cf4501f25d6ac242d1ec83e4c1c70814
darleynartey <barbaradnartey@gmail.com>
2024-02-15 11:38:37 +0000

    set up the project

---
+++ src/components/add-todo/index.js
@@ -0,0 +1,16 @@
+import styles from "./index.css";
+
+function AddTodo() {
+    return (
+        <section className={styles.AddTodo}>
+            <input placeholder="Start typing..."/>
+            <button>Create</button>
+
+
+
+
+        </section>
+    );
+}
+
+export default AddTodo;
---
+++ src/components/todo-list/index.js
@@ -0,0 +1,16 @@
+function TodoList() {
+    return (
+        <ul>
+            <li>Complete online JavaScript course</li>
+            <li>Jog around the park 3 times</li>
+            <li>Jog around the park 3 times</li>
+            <li>Read for 1 hour</li>
+            
+
+
+
+        </ul>
+    );
+}
+
+export default TodoList;
